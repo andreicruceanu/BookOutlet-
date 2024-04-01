06f9ef5301322f1f752bfb997c84676d695c7449
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-02 02:00:40 +0300

    add contetnt text project

---
+++ src/constants/content.js
@@ -0,0 +1,11 @@
+const content = {
+  btn_forgot_Password: "Recuperează parola",
+  enter_the_account: "Intră în cont",
+  add_to_cart: "Adaugă în coș",
+  add_to_favorite: "Adaugă la favorite",
+  remove_to_favorite: "Șterge de la favorite",
+  go_to_login: "Mergi la pagina de login",
+  cancel: "Anulează",
+};
+
+export default content;
