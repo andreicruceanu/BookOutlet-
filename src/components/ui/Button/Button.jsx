f5e8dbd1b7a14cef4a574f6f372ab779ddc7872d
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-02 01:58:03 +0300

    button ui

---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { ImSpinner8 } from "react-icons/im";
+import styles from "./styles.module.css";
+const Button = React.forwardRef(
+  (
+    {
+      className,
+      classNameText,
+      name,
+      startIconImage,
+      startIcon,
+      size = "md",
+      type = "button",
+      variant = "primary",
+      isLoading = false,
+      ...props
+    },
+    ref
+  ) => {
+    const combinedClassName = className
+      ? `${className} ${styles.button} ${styles[size]} ${styles[variant]}`
+      : `${styles.button} ${styles[size]} ${styles[variant]}`;
+
+    return (
+      <button
+        className={combinedClassName}
+        ref={ref}
+        type={type}
+        disabled={isLoading}
+        {...props}
+      >
+        {startIconImage && <img src={startIconImage} alt={name} />}
+        {startIcon && startIcon}
+        {isLoading && (
+          <span className={styles.iconContainer}>
+            <ImSpinner8 />
+          </span>
+        )}
+        {!isLoading && <span className={classNameText}>{name}</span>}
+      </button>
+    );
+  }
+);
+
+export default Button;
