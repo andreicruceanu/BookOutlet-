74b23062c5d65238718537215573752b5b5ed3fe
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-02 18:07:26 +0300

    input radio component

---
+++ src/components/ui/InputRadio/Radio.jsx
@@ -0,0 +1,39 @@
+import styles from "./styles.module.css";
+
+const Radio = ({
+  className,
+  type = "radio",
+  name,
+  label,
+  formik,
+  value,
+  id,
+  ...props
+}) => {
+  const { values, handleChange } = formik;
+
+  const combinedClassName = className
+    ? `${className} ${styles.radio_primary}`
+    : `${styles.radio_primary}`;
+
+  return (
+    <>
+      <input
+        className={combinedClassName}
+        value={value}
+        checked={values.gender.toString() === value.toString()}
+        onChange={handleChange}
+        type={type}
+        name={name}
+        {...props}
+      />
+      <label className={styles.genderLabelMr} htmlFor={id}>
+        {label}
+      </label>
+    </>
+  );
+};
+
+Radio.displayName = "Radio";
+
+export default Radio;
