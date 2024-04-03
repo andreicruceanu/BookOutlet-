2453433065bf3e23c4493350ffab92d0397fe862
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-03 14:27:28 +0300

    form Adrress

---
+++ src/pages/account/Components/FormAddress/FormAddress.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FormAddress = () => {
+  return (
+    <>
+      <div>
+        <form>
+          <h4>Persoana de contant</h4>
+          <div className="row">
+            <div></div>
+            <div></div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormAddress;
