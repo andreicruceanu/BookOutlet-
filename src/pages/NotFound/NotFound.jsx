29367a07215d755f91aba5f0260ffba2ebad796d
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-05 12:02:15 +0300

    add page not Found

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,26 @@
+import { BiErrorCircle } from "react-icons/bi";
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+const NotFound = () => {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.wrap}>
+          <div className={styles.error}>
+            <BiErrorCircle className={styles.errorIcon} />
+            <p className={styles.title}>This page could not be found</p>
+            <Link className={styles.link} to="/">
+              Back to the home page
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
