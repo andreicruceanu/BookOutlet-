00535e040c928096f7c3f0435c7a7aaa46ca2fd0
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-06 15:25:55 +0300

    add modal bo  user in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { setListFavorites } from "./features/auth/authSlice";
+import { setListFavorites, setModalNoUser } from "./features/auth/authSlice";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ModalNoUser from "./components/modalNoUser/modalNoUser";
 
 function App() {
-  const { user } = useSelector((state) => state.auth);
+  const { user, modalNoUser } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const handleClose = () => dispatch(setModalNoUser(false));
+
   useEffect(() => {
     const getFavorites = async () => {
       const res = await axios.get("/api/user/favorite", {
@@ -40,6 +43,14 @@ function App() {
         autoClose="3000"
         closeOnClick
         pauseOnHover={false}
+      />
+      <ModalNoUser
+        textHeader={"Loghează-te"}
+        textContent={
+          "Pentru a putea adauga la favorite o carte trebuie sa intri in contul tau BookOutlet"
+        }
+        open={modalNoUser}
+        onClose={handleClose}
       />
       <Navbar />
       <Outlet />
