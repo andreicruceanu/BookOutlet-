37fceebbf63b11bcbfcc1f780907188c6b747a46
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-15 23:28:38 +0300

    PersonalData

--- src/pages/account/PersonalData/PersonalData.jsx
+++ src/pages/account/PersonalData/PersonalData.jsx
@@ -2,21 +2,49 @@ import styles from "./styles.module.css";
 import FormPersonalData from "../Components/FormPersonalData/FormPersonalData";
 import Container from "./ContainerPersonalData/Container";
 import Button from "../../../components/ui/Button/Button";
-
+import useModal from "../../../hooks/useModal";
+import Modal from "../../../components/modal/Modal";
+import FormAddress from "../Components/FormAddress/FormAddress";
+import useFetch from "../../../hooks/useFetch";
+import Spinner from "../../../components/Spinner/Spinner";
 function PersonalData() {
+  const { isOpenModal, onOpenModal, onCloseModal } = useModal();
+
+  const { data, isLoading, err } = useFetch("/profile");
+
+  if (err) console.log(err);
+
   return (
     <div className={styles.personalDataMain}>
-      <Container title="Date personale">
-        <FormPersonalData />
-      </Container>
-      <Container title="Adresele mele">
-        <Button
-          type="button"
-          variant="secondary"
-          name="Adauga adresa"
-          className="max-w-250"
-        />
-      </Container>
+      {isLoading ? (
+        <Container>
+          <div className={styles.wrapSpinner}>
+            <Spinner />
+          </div>
+        </Container>
+      ) : (
+        <>
+          <Container title="Date personale">
+            <FormPersonalData data={data} />
+          </Container>
+          <Container title="Adresele mele">
+            <Button
+              onClick={onOpenModal}
+              type="button"
+              variant="secondary"
+              name="Adauga adresa"
+              className="max-w-250"
+            />
+          </Container>
+          <Modal
+            isOpen={isOpenModal}
+            onClose={onCloseModal}
+            title="Adaugă o nouă adresă de livrare"
+          >
+            <FormAddress onClose={onCloseModal} />
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
