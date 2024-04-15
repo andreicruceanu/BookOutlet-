eb0474039a44dd8d3afb8918bf9a467ad30f73ff
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-15 23:23:02 +0300

    useFerch hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,43 +1,22 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import publicClient from "../api/client/public.clinet";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
-import { getItem, setItem } from "../utils/localStorage";
+import privateClient from "../api/client/private.client";
 
-const STALE_TIME = 5 * 60 * 1000; // 5 minutes
-
-const useFetch = (id, options) => {
+const useFetch = (url, options) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(undefined);
   const [isLoading, setIsLoading] = useState(true);
-  const storageKey = useMemo(() => "booksFetched", []);
-  const [booksFetched] = useState(getItem(storageKey) || []);
 
   const abortControllerRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const currentTime = new Date().getTime();
-      const cachedData = booksFetched;
-
-      const findBookCached = cachedData.find(
-        (el) => el.data._id.toString() === id.toString()
-      );
-
-      if (
-        findBookCached &&
-        currentTime - findBookCached.lastFetched < STALE_TIME
-      ) {
-        setData(findBookCached.data);
-        setIsLoading(false);
-        return;
-      }
-
       abortControllerRef.current = new AbortController();
 
       setError(null);
       setIsLoading(true);
       try {
-        const response = await publicClient.get(`book/${id}`, {
+        const response = await privateClient.get(url, {
           ...options,
           signal: abortControllerRef.current?.signal,
         });
@@ -56,25 +35,7 @@ const useFetch = (id, options) => {
     return () => {
       abortControllerRef.current?.abort();
     };
-  }, [options, booksFetched, id]);
-
-  useEffect(() => {
-    if (!data) return;
-
-    const newData = { data, lastFetched: new Date().getTime() };
-
-    const existingBook = booksFetched.find(
-      (book) => book.data._id.toString() === data._id.toString()
-    );
-
-    const updatedBooks = existingBook
-      ? booksFetched.map((el) =>
-          el.data._id.toString() === newData.data._id.toString() ? newData : el
-        )
-      : [...booksFetched, newData];
-
-    setItem(storageKey, updatedBooks);
-  }, [data, storageKey, booksFetched]);
+  }, [options, url]);
 
   return { data, error, isLoading };
 };
