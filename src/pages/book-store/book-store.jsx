59d76daa2b3b44ea53069c02461139059d8134c7
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-15 23:29:58 +0300

    book store

--- src/pages/book-store/book-store.jsx
+++ src/pages/book-store/book-store.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BANNERS } from "../../constants/bannerComponentDisplay";
 import styles from "./styles.module.css";
 import Carousel from "../../components/carousel/carousel";
 import CarouselMenu from "../../components/carousel-menu/carousel-menu";
-import Banner from "../../components/banner/Banner";
-import BannerHeader from "../../components/bannerHeader/BannerHeader";
 import Autors from "../../components/authors/Authors";
 import OnlineLibrary from "../../components/sectionOnlineLibrary/OnlineLibrary";
-
-import requests from "../../constants/requests";
+import { useDispatch, useSelector } from "react-redux";
 import InfoHome from "../../components/infoHome/InfoHome";
-
 import SwiperBooks from "../../components/ui/SwiperBooks/SwiperBooks";
+import { getBanners } from "../../features/banners/bannersSlice";
+import Banner from "../../components/banners/banner/Banner";
+import BannerHeader from "../../components/banners/bannerHeader/BannerHeader";
+import { getBooks } from "../../features/Books/BooksSlice";
+import ContainerSwiperBookStore from "../../components/containers/containerBookStore/ContainerSwiperBookStore";
 
 function BookStore() {
+  const {
+    topBooks,
+    newBooks,
+    newsBookoutlet,
+    topPackagesBook,
+    loading,
+    error,
+  } = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const request = dispatch(getBanners());
+
+    return () => {
+      request.abort();
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    const request = dispatch(getBooks());
+
+    return () => {
+      request.abort();
+    };
+  }, [dispatch]);
+
   return (
     <main>
-      <BannerHeader fetchURL={requests.requestBanner} />
+      <BannerHeader />
       <div className={`${styles.container} ${styles.borderContainer}`}>
         <div className={styles.carouselContainer}>
           <CarouselMenu />
@@ -25,28 +52,30 @@ function BookStore() {
       </div>
       <h1 className={styles.homePageTitle}>Librarie Online</h1>
       <Banner type={BANNERS.firstComponent} />
-      <section className={styles.newProducts}>
-        <div className={styles.containerSwiperBooks}>
-          <SwiperBooks title={"Noutăți Bookoutlet"} viewBook={6} />
-        </div>
-      </section>
-      <section className={styles.newProducts}>
-        <div className={styles.containerSwiperBooks}>
-          <SwiperBooks title={"Noutăți"} viewBook={6} />
-        </div>
-      </section>
+      <ContainerSwiperBookStore>
+        <SwiperBooks
+          title={"Noutăți Bookoutlet"}
+          viewBook={6}
+          books={topBooks}
+        />
+      </ContainerSwiperBookStore>
+
+      <ContainerSwiperBookStore>
+        <SwiperBooks title={"Noutăți"} viewBook={6} books={newsBookoutlet} />
+      </ContainerSwiperBookStore>
+
       <OnlineLibrary />
-      <section className={styles.newProducts}>
-        <div className={styles.containerSwiperBooks}>
-          <SwiperBooks title={"Top cărți"} viewBook={6} />
-        </div>
-      </section>
+      <ContainerSwiperBookStore>
+        <SwiperBooks title={"Top cărți"} viewBook={6} books={newBooks} />
+      </ContainerSwiperBookStore>
       <Banner type={BANNERS.secondaryComponent} />
-      <section className={styles.newProducts}>
-        <div className={styles.containerSwiperBooks}>
-          <SwiperBooks viewBook={6} title={"Top pachete de cărți"} />
-        </div>
-      </section>
+      <ContainerSwiperBookStore>
+        <SwiperBooks
+          viewBook={6}
+          title={"Top pachete de cărți"}
+          books={topPackagesBook}
+        />
+      </ContainerSwiperBookStore>
       <Autors />
       <InfoHome />
     </main>
