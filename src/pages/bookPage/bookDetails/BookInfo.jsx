39e22e7600814e8a7efa72c3f027e3f0ae51205e
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-15 23:23:48 +0300

    book Details

--- src/pages/bookPage/bookDetails/BookInfo.jsx
+++ src/pages/bookPage/bookDetails/BookInfo.jsx
@@ -2,11 +2,6 @@ import { Link, useParams } from "react-router-dom";
 import BookInfoCarousel from "../../../components/bookInfoCarousel/BookInfoCarousel.jsx";
 import BookStar from "../../../components/bookStar/BookStar.jsx";
 import styles from "./styles.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import ModalNoUser from "../../../components/modalNoUser/modalNoUser.jsx";
-import { setModalNoUser } from "../../../features/auth/authSlice.js";
-import { toast } from "react-toastify";
-import { useBookDetails } from "../../../hooks/fetch-book-details.js";
 import ListAuthors from "../Components/ListAuthors/ListAuthors.jsx";
 import BookTitle from "../Components/BookTitle/BookTitle.jsx";
 import BookOverview from "../Components/BookOverview/BookOverview.jsx";
@@ -17,106 +12,103 @@ import BookAttributes from "../Components/BookAttributes/BookAttributes.jsx";
 import Reviews from "../Components/Reviews/Reviews.jsx";
 import Devider from "../Components/Devider/Devider.jsx";
 import ButtonsPage from "../Components/ButtonsPage/ButtonsPage.jsx";
+import useScrollTop from "../../../hooks/useScrollTop.js";
+import { toast } from "react-toastify";
+import useRecommendedProducts from "../../../hooks/useRecommendedBooks.js";
+import SwiperBooks from "../../../components/ui/SwiperBooks/SwiperBooks.jsx";
+import BookRating from "../Components/BookRating/BookRating.jsx";
+import Container from "../Components/Container/Container.jsx";
+import ContainerSwiperBookStore from "../../../components/containers/containerBookStore/ContainerSwiperBookStore.jsx";
+import useFetchBooks from "../../../hooks/useFetchBooks.js";
 
 function BookInfo() {
-  const { modalNoUser } = useSelector((state) => state.auth);
-
   const { id } = useParams();
 
-  const { book, error } = useBookDetails(id);
+  const { data: book, isLoading, error } = useFetchBooks(id);
 
-  const dispatch = useDispatch();
+  const {
+    data: recommendedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured,
+  } = useRecommendedProducts(id);
 
-  const handleClose = () => {
-    return dispatch(setModalNoUser(false));
-  };
+  useScrollTop(id);
 
-  if (error) {
-    toast.error(error);
-    return;
-  }
+  if (error) return toast.error(error);
+
   return (
-    <>
-      {book && (
-        <main>
-          <ModalNoUser
-            textHeader={"Loghează-te"}
-            textContent={
-              "Pentru a putea adauga la favorite o carte trebuie sa intri in contul tau BookOutlet"
-            }
-            open={modalNoUser}
-            onClose={handleClose}
-          />
-          <div className={styles.containerBig}>
-            <div className={styles.wrap}>
-              <div className={styles.carousel}>
-                <BookInfoCarousel
-                  badges={book.badges}
-                  bookImg={book.images}
-                  price={book.price}
-                  name={book.title}
-                />
-              </div>
-              <div className={styles.details}>
-                <BookTitle
-                  title={book.title}
-                  subtitle={book.subtitle}
-                  authors={book.authors}
-                />
-                <div className={styles.detailsInfo}>
-                  <div className={styles.detailsInfoRating}>
-                    <div className={styles.detailsInfoStar}>
-                      <BookStar rating={book.rating.rating} />
-                    </div>
-                    <Link to={"reviews"} className={styles.detailsInfoReview}>
-                      {`${
-                        Number.isInteger(book.rating.rating)
-                          ? book.rating.rating
-                          : Number(book.rating.rating).toFixed(2)
-                      } (${book.rating.totalReviews} review-uri)`}
-                    </Link>
-                  </div>
-                  <BookPrice
-                    oldPrice={book?.price.oldPrice}
-                    price={book?.price.price}
-                  />
-                </div>
-                <div className={styles.detailsPricing}>
-                  <BookOverview
-                    shortDescription={book.shortDescription}
-                    promoDescription={book.promoDescription}
-                  />
-                  <div className={styles.detailsPricingBox}>
-                    <ButtonsPage book={book} />
-                    <InfoDelivery />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <Devider />
-            <BookAttributes bookName={book.title} bookId={book.bookId} />
-            <Devider />
-            <div className={styles.container}>
-              <Summary
-                title={book.title}
-                authors={book.authors}
-                description={book.description}
-              />
-            </div>
-            <Devider />
-            <ListAuthors listAuthors={book?.authors} />
-            <Devider />
-            <div className={styles.container}>
-              <Reviews
-                review={book.rating}
-                bookTitle={book.title}
-                bookId={book._id}
-              />
-            </div>
-          </div>
-        </main>
+    <main>
+      {isLoading && (
+        <div className={styles.containerLoader}>
+          <div className={styles.loader}></div>
+        </div>
       )}
-    </>
+      {book && !isLoading && (
+        <div className="flex flex-col">
+          <Container>
+            <BookInfoCarousel
+              badges={book.badges}
+              bookImg={book.images}
+              price={book.price}
+              name={book.title}
+            />
+            <div className="flex flex-col w-full">
+              <BookTitle
+                title={book.title}
+                subtitle={book.subtitle}
+                authors={book.authors}
+              />
+              <div className="flex flex-row mt-10 items-center">
+                <BookRating rating={book.rating} />
+                <BookPrice
+                  oldPrice={book?.price.oldPrice}
+                  price={book?.price.price}
+                />
+              </div>
+              <div className="flex flex-row mt-20">
+                <BookOverview
+                  shortDescription={book.shortDescription}
+                  promoDescription={book.promoDescription}
+                />
+                <div className="w-50">
+                  <ButtonsPage book={book} />
+                  <InfoDelivery />
+                </div>
+              </div>
+            </div>
+          </Container>
+          <Devider />
+          <Container>
+            <BookAttributes bookName={book.title} bookId={book.bookId} />
+          </Container>
+          <Devider />
+          <Container>
+            <Summary
+              title={book.title}
+              authors={book.authors}
+              description={book.description}
+            />
+          </Container>
+          <Devider />
+          <ListAuthors listAuthors={book?.authors} />
+          <Devider />
+          <Container>
+            <Reviews
+              review={book.rating}
+              bookTitle={book.title}
+              bookId={book._id}
+            />
+          </Container>
+          <ContainerSwiperBookStore>
+            <SwiperBooks
+              title={"Recommended for You"}
+              viewBook={6}
+              books={recommendedProducts}
+            />
+          </ContainerSwiperBookStore>
+        </div>
+      )}
+    </main>
   );
 }
 
