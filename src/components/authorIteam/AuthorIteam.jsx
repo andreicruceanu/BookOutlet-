b6054b444b29e0f8234e084422b198b20801034f
andreicruceanu <cruceanuandrei10@gmail.com>
2024-04-24 23:55:56 +0300

    authorItem update

--- src/components/authorIteam/AuthorIteam.jsx
+++ src/components/authorIteam/AuthorIteam.jsx
@@ -1,34 +1,26 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
-import { v4 as uuidv4 } from "uuid";
-import { API_URL_IMG } from "../../api/api-img";
+import { getImageUrl } from "../../utils/images";
 
-function AuthorIteam({ dataAuthor }) {
+function AuthorIteam({ author }) {
   return (
-    dataAuthor.length > 0 && (
-      <div className={styles.authorWrap}>
-        {dataAuthor?.map((author) => (
-          <div className={styles.authorIteam} key={uuidv4()}>
-            <div className={styles.authorIteamImg}>
-              <Link
-                to={`autor/${author.url}`}
-                title={author.value}
-                className={styles.authorLink}
-              >
-                <img
-                  src={`${API_URL_IMG}${author.imageUrl}`}
-                  alt={author.value}
-                />
-              </Link>
-            </div>
-            <h2 className={styles.authorIteamTitle}>
-              <Link to={`autor/${author.url}`} title={author.value}>
-                {author.value}
-              </Link>
-            </h2>
-            <p className={styles.authorIteamText}>{author.books} produse</p>
-          </div>
-        ))}
+    author && (
+      <div className={styles.authorIteam}>
+        <div className={styles.authorIteamImg}>
+          <Link
+            to={`autor/${author.url}`}
+            title={author.value}
+            className={styles.authorLink}
+          >
+            <img src={getImageUrl(author.imageUrl)} alt={author.value} />
+          </Link>
+        </div>
+        <h2 className={styles.authorIteamTitle}>
+          <Link to={`autor/${author.url}`} title={author.value}>
+            {author.value}
+          </Link>
+        </h2>
+        <p className={styles.authorIteamText}>{author.books} produse</p>
       </div>
     )
   );
