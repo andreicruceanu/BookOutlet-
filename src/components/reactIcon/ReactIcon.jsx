f523877eb5e3b939a01d9940514e3fdd97a6e741
andreicruceanu <cruceanuandrei10@gmail.com>
2024-06-05 01:30:55 +0300

    refactoring styles

--- src/components/reactIcon/ReactIcon.jsx
+++ src/components/reactIcon/ReactIcon.jsx
@@ -5,8 +5,7 @@ import React from "react";
 
 const ReactIcon = ({ icons }) => {
   const iconFolder = icons.slice(0, 2).toLowerCase();
-  // Map Icon to Respective Folder by comparing the First two Character of the
-  //Icons
+
   let icon =
     iconFolder === "ai"
       ? ReactIconAI[icons]
@@ -15,7 +14,7 @@ const ReactIcon = ({ icons }) => {
       : iconFolder === "bs"
       ? ReactIconBs[icons]
       : "";
-  // return only if the icon Exist
+
   return icon && React.createElement(icon);
 };
 
